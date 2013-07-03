c3b265b60cd34826bdde6fcb74058904f3a8302d
Dan Tao <daniel.tao@gmail.com>
2013-07-03 15:35:15 -0700

    moved logic to apply an operation to a list into NaiveListBuilder<T> class

--- ListBuilder/NaiveListBuilder.cs
+++ ListBuilder/NaiveListBuilder.cs
@@ -10,9 +10,24 @@ namespace ListBuilder
             var list = new List<T>();
             foreach (Op<T> operation in operations)
             {
-                operation.Apply(list);
+                Apply(operation, list);
             }
             return list;
+        }
+        
+        static void Apply(Op<T> op, IList<T> list)
+        {
+            switch (op.Type)
+            {
+            case OpType.Insert:
+                list.Insert(op.Index, op.Value);
+                break;
+            case OpType.Remove:
+                list.RemoveAt(op.Index);
+                break;
+            default:
+                throw new ArgumentException(string.Format("Invalid operation: {0}", op.Type));
+            }
         }
     }
 }
--- ListBuilder/Op.cs
+++ ListBuilder/Op.cs
@@ -10,23 +10,9 @@ namespace ListBuilder
         public T Value;
     }
 
+    // Helper class with convenient methods for creating Op<T> values
     public static class Op
     {
-        public static void Apply<T>(this Op<T> op, IList<T> list)
-        {
-            switch (op.Type)
-            {
-            case OpType.Insert:
-                list.Insert(op.Index, op.Value);
-                break;
-            case OpType.Remove:
-                list.RemoveAt(op.Index);
-                break;
-            default:
-                throw new ArgumentException(string.Format("Invalid operation: {0}", op.Type));
-            }
-        }
-
         public static Op<T> Insert<T>(int index, T value)
         {
             return new Op<T>
